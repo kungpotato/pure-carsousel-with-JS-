dda5445704a4c19b4a8700b0ba85cb8031061cbb
kungpotato <kittisak.py@gmail.com>
2017-07-17 10:59:13 +0700

    first com

---
+++ js/slide.js
@@ -0,0 +1,23 @@
+var a = 0; // a = slide index
+var myVar;
+slide()
+
+function startSlide(){
+     myVar = setInterval(slide,1000);
+}
+
+function slide(){
+    var x = document.getElementsByClassName("my_slide");
+   //  console.log(x.length)
+   for(i = 0; i < x.length; i++){
+        x[i].style.display = "none"; // hide all img
+   }
+    a++;
+    if(a > x.length){a = 1}
+    x[a-1].style.display = "block";
+    // setTimeout(slide, 8000);
+}
+
+function stopSlide() {
+  clearInterval(myVar);
+}
